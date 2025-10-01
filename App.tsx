d2891e8eecf1828bff2713d03131bd1a06ee9e76
Artem-Kalugin <Aharlo4@gmail.com>
2025-10-01 13:21:41 +0300

    [HLPL-0] feat: configure reanimated & navigation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { PersistGate } from 'redux-persist/integration/react';
 
@@ -10,10 +14,24 @@ export default function App() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <View style={styles.container}>
-          <Text>Open up App.tsx to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
+        <SafeAreaProvider>
+          <GestureHandlerRootView style={StyleSheet.absoluteFill}>
+            <NavigationContainer
+              theme={{
+                ...DefaultTheme,
+                colors: {
+                  ...DefaultTheme.colors,
+                  background: 'transparent',
+                },
+              }}
+            >
+              <View style={styles.container}>
+                <Text>Open up App.tsx to start working on your app!</Text>
+                <StatusBar style="auto" />
+              </View>
+            </NavigationContainer>
+          </GestureHandlerRootView>
+        </SafeAreaProvider>
       </PersistGate>
     </Provider>
   );
