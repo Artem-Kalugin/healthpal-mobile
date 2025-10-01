15ddfe195e344a1f729a431d51c6cffff2133afa
Artem-Kalugin <Aharlo4@gmail.com>
2025-10-01 13:07:15 +0300

    [HLPL-0] feat: configure storage

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 import { StatusBar } from 'expo-status-bar';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { persistor, store } from '#store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </PersistGate>
+    </Provider>
   );
 }
 
