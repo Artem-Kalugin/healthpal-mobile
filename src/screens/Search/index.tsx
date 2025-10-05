404f0f4d3458f94f85c16b562c2d2c5a3a492198
Artem-Kalugin <Aharlo4@gmail.com>
2025-10-05 23:10:44 +0300

    [HLPL-8] feat: add search header animation

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,5 +1,18 @@
 import React, { useRef, useState } from 'react';
-import { FlatList, StyleSheet, TouchableOpacity, View } from 'react-native';
+import {
+  FlatList,
+  Keyboard,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import Animated, {
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 import { DoctorCard } from '#components/entities/Doctor/Card';
 import HeaderWithThreeSections from '#components/HeaderWithThreeSections';
@@ -10,77 +23,117 @@ import { Tag } from '#ui-kit/Tag';
 
 import { MainRoutes, MainScreenProps } from '#navigation/Main/types';
 
-import { colors } from '#config';
+import { colors, shadow } from '#config';
 
 export const Search: React.FC<MainScreenProps<MainRoutes.Search>> = props => {
   const categoriesScrollRef = useRef<FlatList>(null);
+  const [foldableContainerHeight, setFoldableContainerHeight] = useState(0);
+  const foldProgress = useSharedValue(1);
+  const previousScrollPositionRef = useRef(0);
 
   const [activeCategory, setActiveCategory] = useState('all');
+
+  const listPaddingToPreventOverlaying = foldableContainerHeight;
+
+  const handler = useAnimatedScrollHandler({
+    onScroll: event => {
+      const nextProgressValueByOffset = +(
+        event.contentOffset.y < foldableContainerHeight
+      );
+
+      const nextProgressValueByVelocity =
+        event.contentOffset.y - previousScrollPositionRef.current < 0 ? 1 : 0;
+
+      foldProgress.value = withTiming(
+        nextProgressValueByVelocity || nextProgressValueByOffset,
+      );
+
+      previousScrollPositionRef.current = event.contentOffset.y;
+    },
+  });
+
+  const rFoldable = useAnimatedStyle(() => ({
+    transform: [
+      { translateY: (foldProgress.value - 1) * foldableContainerHeight },
+    ],
+  }));
 
   return (
     <>
       <View>
         <HeaderWithThreeSections
-          containerStyle={styles.searchBarContainer}
+          containerStyle={styles.headerContainer}
           title="Все специалисты"
           titleTextAlign="center"
         />
 
-        <View style={styles.searchBarContainer}>
-          <TextInput
-            autoFocus
-            containerStyle={styles.searchBar}
-            IconLeft={<Icon name="search" />}
-            placeholder="Поиск врача…"
-            size="small"
-          />
-        </View>
-
-        <FlatList
-          ref={categoriesScrollRef}
-          horizontal
-          contentContainerStyle={styles.tagsContainer}
-          data={[
-            'All',
-            'Cardiologists',
-            'Dentists',
-            'Pulmonology',
-            'Another category',
-          ]}
-          renderItem={({ item: el, index }) => (
-            <Tag
-              key={el}
-              active={el === activeCategory}
-              value={el}
-              onPress={() => {
-                setActiveCategory(el);
-                categoriesScrollRef.current?.scrollToIndex({
-                  index,
-                  viewPosition: 0.5,
-                });
-              }}
-            />
-          )}
-          showsHorizontalScrollIndicator={false}
-          style={styles.tagsList}
-        />
-
-        <View style={styles.sectionHeader}>
-          <TextBase weight="700">532 founds</TextBase>
-          <TouchableOpacity
-            onPress={() => props.navigation.navigate(MainRoutes.Search)}
+        <View style={styles.foldableContainerPlaceholder}>
+          <Animated.View
+            style={[styles.foldableContainer, rFoldable, shadow]}
+            onLayout={el =>
+              setFoldableContainerHeight(el.nativeEvent.layout.height)
+            }
           >
-            <TextSmall
-              color={colors.grayscale['500']}
-              weight="500"
-            >
-              Все
-            </TextSmall>
-          </TouchableOpacity>
+            <View style={styles.searchBarContainer}>
+              <TextInput
+                autoFocus
+                containerStyle={styles.searchBar}
+                IconLeft={<Icon name="search" />}
+                placeholder="Поиск врача…"
+                size="small"
+              />
+            </View>
+            <FlatList
+              ref={categoriesScrollRef}
+              horizontal
+              contentContainerStyle={styles.tagsContainer}
+              data={[
+                'All',
+                'Cardiologists',
+                'Dentists',
+                'Pulmonology',
+                'Another category',
+              ]}
+              renderItem={({ item: el, index }) => (
+                <Tag
+                  key={el}
+                  active={el === activeCategory}
+                  value={el}
+                  onPress={() => {
+                    setActiveCategory(el);
+                    categoriesScrollRef.current?.scrollToIndex({
+                      index,
+                      viewPosition: 0.5,
+                    });
+                  }}
+                />
+              )}
+              showsHorizontalScrollIndicator={false}
+              style={styles.tagsList}
+            />
+
+            <View style={styles.sectionHeader}>
+              <TextBase weight="700">532 founds</TextBase>
+              <TouchableOpacity
+                onPress={() => props.navigation.navigate(MainRoutes.Search)}
+              >
+                <TextSmall
+                  color={colors.grayscale['500']}
+                  weight="500"
+                >
+                  Все
+                </TextSmall>
+              </TouchableOpacity>
+            </View>
+          </Animated.View>
         </View>
       </View>
-      <FlatList
-        data={[1, 2, 3, 4, 5, 6]}
+      <Animated.FlatList
+        contentContainerStyle={{
+          paddingTop: listPaddingToPreventOverlaying,
+        }}
+        data={[1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6]}
+        keyboardShouldPersistTaps="never"
         ListFooterComponent={<ListExtender height={36} />}
         renderItem={() => (
           <View style={styles.cardWrapper}>
@@ -88,6 +141,8 @@ export const Search: React.FC<MainScreenProps<MainRoutes.Search>> = props => {
           </View>
         )}
         style={styles.container}
+        onScroll={handler}
+        onScrollBeginDrag={() => Keyboard.dismiss()}
       />
     </>
   );
@@ -97,6 +152,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     gap: 12,
+  },
+  foldableContainerPlaceholder: {},
+  foldableContainer: {
+    position: 'absolute',
+    zIndex: 1,
+    backgroundColor: colors.white,
+  },
+  headerContainer: {
+    zIndex: 2,
+    paddingHorizontal: 24,
+    backgroundColor: colors.white,
   },
   searchBarContainer: {
     paddingHorizontal: 24,
@@ -121,7 +187,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
   },
   cardWrapper: {
-    marginBottom: 8,
+    paddingTop: 8,
     paddingHorizontal: 24,
   },
 });
