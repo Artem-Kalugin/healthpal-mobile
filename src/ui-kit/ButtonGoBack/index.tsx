c4646e897c9dca06ce55fa6caa97b7f5132fbb5b
Artem-Kalugin <Aharlo4@gmail.com>
2025-11-03 22:00:43 +0300

    [HLPL-42] feat: add buttonGoBack tests

--- src/ui-kit/ButtonGoBack/index.tsx
+++ src/ui-kit/ButtonGoBack/index.tsx
@@ -7,14 +7,18 @@ import { Icon } from '#ui-kit/Icon';
 
 import { colors, hitSlopBig } from '#config';
 
+import { ButtonGoBackTestIds } from './config';
+
 export interface IButtonGoBack {
   onPress: () => void;
   style: StyleProp<ViewStyle>;
+  testIdConfig: typeof ButtonGoBackTestIds;
 }
 
 const ButtonGoBack: React.FC<Partial<IButtonGoBack>> = ({
   onPress = undefined,
   style,
+  testIdConfig = ButtonGoBackTestIds,
 }) => {
   const navigation = useNavigation();
 
@@ -22,6 +26,7 @@ const ButtonGoBack: React.FC<Partial<IButtonGoBack>> = ({
     <TouchableOpacity
       hitSlop={hitSlopBig}
       style={style}
+      testID={testIdConfig.root}
       onPress={onPress || navigation.goBack}
     >
       <Icon
